2a4d1397fda1bb944fb05e9541e840c2c286f75a
after7203 <after7203@naver.com>
2023-02-28 14:26:34 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,46 +47,76 @@ export type MatchType = {
 
 export const getServerSideProps: GetServerSideProps = async () => {
   let matches = null
-  try {
-    console.log(-1)
-    const puppeteer = require("puppeteer");
-    console.log(0)
-    const browser = await puppeteer.launch();
-    console.log(1)
-    const page = await browser.newPage();
-    console.log(2)
-    await page.goto("https://game.naver.com/esports/League_of_Legends/schedule/lck");
-    console.log(3)
-    const content = await page.content()
-    console.log(4)
-    const $ = cheerio.load(content)
-    console.log(5)
-    const lists = $(".card_item__3Covz").toArray();
-    matches = JSON.stringify(lists.map((list) => {
-      return {
-        date: $(list).find(".card_date__1kdC3").text(),
-        day_matches: $(list).find(".row_item__dbJjy").toArray().map(el => {
-          return {
-            time: $(el).find(".row_time__28bwr").text(),
-            state: $(el).find(".row_state__2RKDU").text(),
-            title: $(el).find(".row_title__1sdwN").text(),
-            home: $(el).find(".row_home__zbX5s .row_name__IDFHz").text(),
-            home_score: $(el).find(".row_home__zbX5s .row_score__2RmGQ").text(),
-            away: $(el).find(".row_away__3zJEV .row_name__IDFHz").text(),
-            away_score: $(el).find(".row_away__3zJEV .row_score__2RmGQ").text()
-          }
-        })
-      }
-    }));
-  } catch (error) {
-    console.log(error);
+  console.log(-1)
+  const puppeteer = require("puppeteer");
+  console.log(0)
+  const browser = await puppeteer.launch();
+  console.log(1)
+  const page = await browser.newPage();
+  console.log(2)
+  await page.goto("https://game.naver.com/esports/League_of_Legends/schedule/lck");
+  console.log(3)
+  const content = await page.content()
+  console.log(4)
+  const $ = cheerio.load(content)
+  console.log(5)
+  const lists = $(".card_item__3Covz").toArray();
+  matches = JSON.stringify(lists.map((list) => {
     return {
-      props: {
-        matches,
-        error
-      }
+      date: $(list).find(".card_date__1kdC3").text(),
+      day_matches: $(list).find(".row_item__dbJjy").toArray().map(el => {
+        return {
+          time: $(el).find(".row_time__28bwr").text(),
+          state: $(el).find(".row_state__2RKDU").text(),
+          title: $(el).find(".row_title__1sdwN").text(),
+          home: $(el).find(".row_home__zbX5s .row_name__IDFHz").text(),
+          home_score: $(el).find(".row_home__zbX5s .row_score__2RmGQ").text(),
+          away: $(el).find(".row_away__3zJEV .row_name__IDFHz").text(),
+          away_score: $(el).find(".row_away__3zJEV .row_score__2RmGQ").text()
+        }
+      })
     }
-  }
+  }));
+  // try {
+  //   console.log(-1)
+  //   const puppeteer = require("puppeteer");
+  //   console.log(0)
+  //   const browser = await puppeteer.launch();
+  //   console.log(1)
+  //   const page = await browser.newPage();
+  //   console.log(2)
+  //   await page.goto("https://game.naver.com/esports/League_of_Legends/schedule/lck");
+  //   console.log(3)
+  //   const content = await page.content()
+  //   console.log(4)
+  //   const $ = cheerio.load(content)
+  //   console.log(5)
+  //   const lists = $(".card_item__3Covz").toArray();
+  //   matches = JSON.stringify(lists.map((list) => {
+  //     return {
+  //       date: $(list).find(".card_date__1kdC3").text(),
+  //       day_matches: $(list).find(".row_item__dbJjy").toArray().map(el => {
+  //         return {
+  //           time: $(el).find(".row_time__28bwr").text(),
+  //           state: $(el).find(".row_state__2RKDU").text(),
+  //           title: $(el).find(".row_title__1sdwN").text(),
+  //           home: $(el).find(".row_home__zbX5s .row_name__IDFHz").text(),
+  //           home_score: $(el).find(".row_home__zbX5s .row_score__2RmGQ").text(),
+  //           away: $(el).find(".row_away__3zJEV .row_name__IDFHz").text(),
+  //           away_score: $(el).find(".row_away__3zJEV .row_score__2RmGQ").text()
+  //         }
+  //       })
+  //     }
+  //   }));
+  // } catch (error) {
+  //   console.log(error);
+  //   return {
+  //     props: {
+  //       matches,
+  //       error
+  //     }
+  //   }
+  // }
   return {
     props: {
       matches
